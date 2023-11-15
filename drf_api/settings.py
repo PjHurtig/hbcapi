d501cabcc9eb57a77993e7560a20cadda93fa534
PjHurtig <>
2023-11-15 13:56:53 +0000

    add pagination, datetime format and naturaltime

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -29,10 +29,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
-     			if 'DEV' in os.environ
-     			else 'dj_rest_auth.jwt_auth.jwtCookieAuthentication'
-    )]
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
